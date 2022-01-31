6432135197a71a4b8a4fd7b2db7ff0d9e399258d
MohammadRezaNouri <MohammadRezaNouri01@Gmail.Com>
2022-02-01 03:26:47 +0330

    Implementing the dequeue in queue.cpp

--- queue.cpp
+++ queue.cpp
@@ -15,3 +15,22 @@ void queue::enqueue(int d)
         rear = d;
     }
 }
+
+void queue::dequeue() // remove in queue (front)
+{
+    if (s1.empty())
+        throw std::invalid_argument("Queue is empty!");
+    while (!s1.empty())
+    {
+        s2.push(s1.top());
+        s1.pop();
+    }
+    s2.pop();
+    if (!s2.empty())
+        front = s2.top();
+    while (!s2.empty())
+    {
+        s1.push(s2.top());
+        s2.pop();
+    }
+}
