0ad33248c2930009c643cb046479ca73d49156df
MohammadRezaNouri <MohammadRezaNouri01@Gmail.Com>
2022-02-01 03:12:33 +0330

    Definition of class queue in queue.h

--- queue.h
+++ queue.h
@@ -1,4 +1,18 @@
 #ifndef QUEUE_H
 #define QUEUE_H
+#include <stack>
+
+class queue
+{
+private:
+    std::stack<int> s1;
+    std::stack<int> s2;
+    int front, rear; // Data in start & last
+public:
+    void enqueue(int); // Insert in queue
+    void dequeue();    // Remove in queue (front)
+    int getF();        // Get front
+    int getR();        // Get rear
+};
 
 #endif
